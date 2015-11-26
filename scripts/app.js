85da67f58c6870199af298c421e60be56ab7e22f
Jesse Zelaya <jessezelaya1@gmail.com>
2015-11-25 16:54:59 -0800

    Added mock todo data

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,13 @@ angular.module('todoListApp', [])
       $scope.helloWorld = function(){
         console.log('Hello there! This is the helloWorld function in the mainCtrl');
       }
-    });
+
+      $scope.todos = [
+        {"name" : "clean the house"},
+        {"name" : "water the dog"},
+        {"name" : "pay dem bills"},
+        {"name" : "run"},
+        {"name" : "swim"}
+      ]
+
+    });//End mainCtrl
