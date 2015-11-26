fd449eacdd0131cdd30cbec75a8a98c7ec0d20e6
Jesse Zelaya <jessezelaya1@gmail.com>
2015-11-25 18:33:21 -0800

    Added service to get mock data

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,29 @@
 angular.module('todoListApp', [])
-    .controller('mainCtrl', function($scope){
+    .controller('mainCtrl', function($scope, dataService){
+
+      $scope.helloConsole = dataService.helloConsole
+
       $scope.change = function(){
         console.log('An input has been changed');
       }
 
-      $scope.todos = [
-        {"name" : "clean the house"},
-        {"name" : "water the dog"},
-        {"name" : "pay dem bills"},
-        {"name" : "run"},
-        {"name" : "swim"}
-      ]
+      dataService.getTodos(function(response){
+        console.log(response.data);
+          $scope.todos = response.data
+      })
 
-    });//End mainCtrl
+
+    })//End mainCtrl
+    .service('dataService', function($http){
+
+      this.helloConsole = function(){
+        console.log('This is the hello console service!');
+      }
+
+      this.getTodos = function(callback){
+        $http.get('mock/todos.json')
+          .then(callback)
+        }
+
+
+    });//End dataService
